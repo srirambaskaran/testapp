eb720a27685585ce0b7ddb7b23f3573c715646e6
Sriram Baskaran <sbaskara@usc.edu>
2019-04-05 22:53:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/test', function (req, res) {
+  res.send('Hello Test!');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
